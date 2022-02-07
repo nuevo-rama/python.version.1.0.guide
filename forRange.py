232cc0e6d8f51b14a65e414a2ded5bf4eb4d0d99
Ramiro Melero <ramiromelero@gmail.com>
2022-02-07 00:30:32 -0300

    Create forRange.py

---
+++ forRange.py
@@ -0,0 +1,11 @@
+# Sumar los valores de una lista sin sumar el último
+# Almacenar el resultado en una variable
+# Mostrar el resultado de la variable
+
+lista = [1,2,3,4]
+
+result = 0
+for i in lista [0:-1]:
+    result = i + result
+
+print (result)
