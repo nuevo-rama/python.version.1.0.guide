2febe3f0258e1827fad9a4a359f03398a342e738
Ramiro Melero <ramiromelero@gmail.com>
2022-02-20 19:44:14 -0300

    Create whileLoop.py

---
+++ whileLoop.py
@@ -0,0 +1,5 @@
+numero = input("Escribe un número para salir del 0 al 10: ")
+
+while numero != str(0):
+    print ("El número:", numero, "es incorrecto")
+    numero = input("Vuelve a escribir otro número: ")
