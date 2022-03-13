908e15d7b987fcdfccf051f0cf2c6bac5e930c3f
Ramiro Melero <ramiromelero@gmail.com>
2022-03-13 19:56:31 -0300

    Create oop.py

---
+++ oop.py
@@ -0,0 +1,10 @@
+class Options:
+    def __init__(self, a, b, c):
+        self._a = a
+        self._b = b
+        self._c = c
+
+    def showOptions(self):
+        print(self._a)
+        print(self._b)
+        print(self._c)
